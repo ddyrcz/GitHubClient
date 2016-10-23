4a9d860ce316f6ec55c6ebb700424bf17cb8042a
Dawid Dyrcz <dyrcu15@gmail.com>
2016-10-23 18:21:57 +0200

    Change files location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search/search.component';
-import { InfoComponent } from './detail/info/info/info.component';
-import { ReposComponent } from './detail/repos/repos/repos.component';
-import { AvatarComponent } from './detail/avatar/avatar/avatar.component';
+import { SearchComponent } from './search/search.component';
+import { InfoComponent } from './detail/info/info.component';
+import { ReposComponent } from './detail/repos/repos.component';
+import { AvatarComponent } from './detail/avatar/avatar.component';
 
 @NgModule({
   declarations: [
