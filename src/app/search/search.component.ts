692ba63979e6322b3d594662b1bb22a4a90c331f
Dawid Dyrcz <dyrcu15@gmail.com>
2016-10-23 19:07:27 +0200

    Implement simply form

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../model/user';
+
 @Component({
   selector: 'search',
   templateUrl: './search.component.html',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  loginType: string[] = ['Normal', 'Facebook',
+    'LinkedIn'];
+
+  user: User = new User('ddyrcz', 'dd', 'Dawid');
+
+  submitted: boolean = false;
+  onSubmit() { this.submitted = true; }
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.user); }
+
+
 
 }
