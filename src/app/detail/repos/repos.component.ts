1f37fe8f7854287e14d0e8fe569084d921a1a5b4
Dawid Dyrcz <dyrcu15@gmail.com>
2016-11-03 18:14:12 +0100

    Add bootstrap to project

--- src/app/detail/repos/repos.component.ts
+++ src/app/detail/repos/repos.component.ts
@@ -23,9 +23,6 @@ export class ReposComponent implements OnInit {
       new Repo('Connections', 'Social web page based on Angular2 and Node.js'),
       new Repo('Weather', 'Weather system using Dark Sky API'),
       new Repo('Dashboard', 'Dashboard page for observing connected computes\' OS parameters, such as free memory available'),
-      new Repo('Connections', 'Social web page based on Angular2 and Node.js'),
-      new Repo('Weather', 'Weather system using Dark Sky API'),
-      new Repo('Dashboard', 'Dashboard page for observing connected computes\' OS parameters, such as free memory available')
     ]
   }
 
